605b85ac0aba4f8c3722bd72c2692ac392e0cc69
Гліб Подлпипський <158773429+Hyperlivus@users.noreply.github.com>
2025-11-18 15:08:23 +0200

    fix lab3

--- lab_3/transactions.sql
+++ lab_3/transactions.sql
@@ -3,9 +3,14 @@ INSERT INTO chat (name, tag, description) VALUES('Chat', 'chat', 'Simple-dimple 
 
 SELECT * FROM chat WHERE tag = 'chat';
 SELECT * FROM client WHERE id = 0;
+SELECT * FROM client WHERE email = 'simple@gmail.com';
+SELECT * FROM chat WHERE name ILIKE '%simple%' OR tag ILIKE '%simple%';
 
 UPDATE client SET nickname = 'OtherSimpleName' WHERE id = 0;
-UPDATE chat SET metedata = metadata || '_new' WHERE NOW() - created_at > INTERVAL '30 seconds';
+UPDATE chat SET metadata = metadata || '_new' WHERE NOW() - created_at > INTERVAL '30 seconds';
+UPDATE chat SET metadata = metadata || '_abandonmen' WHERE NOW() - last_activity_at > INTERVAL '30 days';
+UPDATE chat SET last_activity_at = NOW();
+
 
 DELETE FROM chat WHERE NOW() - last_activity_at > INTERVAL '7 days';
 DELETE FROM client WHERE nickname ILIKE '%simple%' OR tag ILIKE '%simple%';
