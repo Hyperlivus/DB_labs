32093fc26d7bb20a73d526c141409b75a15c893e
Гліб Подлпипський <158773429+Hyperlivus@users.noreply.github.com>
2025-12-01 21:59:20 +0200

    lab6 init

--- lab_4/operations.sql
+++ lab_4/operations.sql
@@ -1,4 +1,3 @@
-
 -- кількість та тип реакції на повідомлення
 SELECT 
     rt.tag,
@@ -11,7 +10,7 @@ WHERE r.message_id = 2
 GROUP BY rt.tag, rt.icon_url, rt.label
 ORDER BY reaction_count DESC;
 
--- сортуємо повідомлення за рейтингом
+-- сортуємо повідомлення за рейтингом (беремо тільки з позитивним рейтингом)
 SELECT msg.id, msg.content, COALESCE(SUM(rt.rate), 0.0) AS rating
 FROM message msg
 LEFT JOIN reaction r ON r.message_id = msg.id
@@ -19,9 +18,10 @@ LEFT JOIN reaction_type rt ON r.type_id = rt.id
 LEFT JOIN member mb ON msg.creator_id = mb.id
 WHERE mb.chat_id = 4
 GROUP BY msg.id
+HAVING COALESCE(SUM(rt.rate), 0.0) > 0.0
 ORDER BY rating DESC;
 
--- виводимо юзерів повідомлення яких мають найбільший рейтинг
+-- виводимо юзерів в чаті повідомлення яких мають найбільший рейтинг
 SELECT mb.id, cln.nickname, cln.tag, COALESCE(SUM(rt.rate)) AS rating
 FROM member mb
 LEFT JOIN client cln ON mb.client_id = cln.id
@@ -32,7 +32,6 @@ WHERE mb.chat_id = 4
 GROUP BY mb.id
 ORDER BY rating
 
-
 -- сортуємо всі чати в яких є юзер за часом їх створення
 SELECT mmb.*, cht.tag, cht.name, cht.avatar_url, cht.description, cht.created_at FROM member mmb
 LEFT JOIN chat cht ON mmb.chat_id = cht.id
@@ -40,3 +39,11 @@ WHERE mmb.client_id = 4
 GROUP BY mmb.id, cht.id
 ORDER BY cht.created_at
 
+-- виводимо три найпопулярніші реакції в чаті
+SELECT rt.id, rt.label, COUNT(r.id) as react_count
+FROM reaction_type rt
+LEFT JOIN member mb ON mb.chat_id = 4
+LEFT JOIN reaction r ON r.owner_id = mb.id AND r.type_id = rt.id
+GROUP BY rt.id
+HAVING COUNT(r.id) > 0.0
+ORDER BY react_count DESC
